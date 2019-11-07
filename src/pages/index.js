a7516c3f4bfc9180e955827259d71949c230edc1
Aydrian Howard <aydrian@gmail.com>
2019-11-07 12:19:15 -0500

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import React from "react"
 import { Grid, Placeholder } from "semantic-ui-react"
 
 import Layout from "../components/layout"
-//import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
