70447773d863c3c60875daf69cb32be5ea29c1c7
Aydrian Howard <aydrian@gmail.com>
2019-11-15 12:10:55 -0500

    Added season jump nav

--- src/templates/season.js
+++ src/templates/season.js
@@ -1,8 +1,17 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 import Layout from "../components/layout"
-import Season from "../components/season"
-import { Breadcrumb } from "semantic-ui-react"
+import { Header, Icon, Breadcrumb, List } from "semantic-ui-react"
+
+import Details from "../components/season-details"
+import Soundtrack from "../components/soundtrack"
+import Press from "../components/press"
+import CastList from "../components/castlist"
+
+const Section = styled.section`
+  margin-top: 1rem;
+`
 
 export const query = graphql`
   query($seasonId: String!) {
@@ -63,19 +72,80 @@ export const query = graphql`
     }
   }
 `
-const SeasonTemplate = ({ data: { seasonsJson: season } }) => (
-  <Layout>
-    <Breadcrumb>
-      <Breadcrumb.Section as={Link} to="/seasons" link>
-        Seasons
-      </Breadcrumb.Section>
-      <Breadcrumb.Divider />
-      <Breadcrumb.Section active>
-        {season.season} ({season.year})
-      </Breadcrumb.Section>
-    </Breadcrumb>
-    <Season {...season} />
-  </Layout>
-)
+const SeasonTemplate = ({ data: { seasonsJson } }) => {
+  const {
+    season,
+    year,
+    imdb,
+    trailer,
+    stream,
+    purchase,
+    cast,
+    soundtrack,
+    press,
+    synopsis,
+  } = seasonsJson
+  return (
+    <Layout>
+      <Breadcrumb>
+        <Breadcrumb.Section as={Link} to="/seasons" link>
+          Seasons
+        </Breadcrumb.Section>
+        <Breadcrumb.Divider />
+        <Breadcrumb.Section active>
+          {season} ({year})
+        </Breadcrumb.Section>
+      </Breadcrumb>
+      <List horizontal bulleted floated="right">
+        {cast && (
+          <List.Item>
+            <a href="#cast">Cast</a>
+          </List.Item>
+        )}
+        {soundtrack && (
+          <List.Item>
+            <a href="#soundtrack">Soundtrack</a>
+          </List.Item>
+        )}
+        {press && (
+          <List.Item>
+            <a href="#press">Press</a>
+          </List.Item>
+        )}
+      </List>
+      <Header as="h1">
+        Season {season} ({year}){" "}
+        <a href={imdb}>
+          <Icon name="imdb" />
+        </a>
+      </Header>
+      <Details
+        season={season}
+        synopsis={synopsis}
+        trailer={trailer}
+        stream={stream}
+        purchase={purchase}
+      />
+      {cast && (
+        <Section id="cast">
+          <Header as="h2">Cast</Header>
+          <CastList cast={cast} />
+        </Section>
+      )}
+      {soundtrack && (
+        <Section id="soundtrack">
+          <Header as="h2">Soundtrack</Header>
+          <Soundtrack songs={soundtrack} />
+        </Section>
+      )}
+      {press && (
+        <Section id="press">
+          <Header as="h2">Press</Header>
+          <Press articles={press} />
+        </Section>
+      )}
+    </Layout>
+  )
+}
 
 export default SeasonTemplate
