1f323b796f2dfa6f7287e61dd201000bfd1d4942
Aydrian Howard <aydrian@gmail.com>
2019-11-24 10:39:26 -0500

    added social sharing buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import styled from "styled-components"
-import { Header, Embed, Responsive } from "semantic-ui-react"
-import { trackCustomEvent } from "gatsby-plugin-google-analytics"
+import {
+  Header,
+  Embed,
+  Responsive,
+  Button,
+  Icon,
+  Container,
+} from "semantic-ui-react"
+import { trackCustomEvent, OutboundLink } from "gatsby-plugin-google-analytics"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -77,7 +84,9 @@ const IndexPage = () => {
       <SEO title="Home" />
       <Wrapper>
         <Featured>
-          <Header>Season {featuredSeason.season}</Header>
+          <Header as="h1">
+            Watch the Season {featuredSeason.season} Trailer
+          </Header>
           <Embed
             id={featuredSeason.trailer.id}
             source={featuredSeason.trailer.source}
@@ -93,13 +102,93 @@ const IndexPage = () => {
               })
             }}
           />
-          <p>{featuredSeason.synopsis}</p>
+          <Container style={{ paddingTop: "0.5em" }}>
+            <Button
+              as="a"
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                "Watch the trailer for Eastsiders Season 4 available to stream on @Netflix beginning December 1."
+              )}&url=${encodeURIComponent(
+                "https://www.youtube.com/watch?v=hdoeDtY6XQc"
+              )}&via=${"eastsiderstv"}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={e => {
+                trackCustomEvent({
+                  // string - required - The object that was interacted with (e.g.video)
+                  category: "Trailer",
+                  // string - required - Type of interaction (e.g. 'play')
+                  action: "Share on Twitter",
+                  // string - optional - Useful for categorizing events (e.g. 'Spring Campaign')
+                  label: `Season ${featuredSeason.season}`,
+                })
+              }}
+              color="twitter"
+              size="tiny"
+            >
+              <Icon name="twitter" /> Share on Twitter
+            </Button>
+            <Button
+              as="a"
+              href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+                "https://www.facebook.com/EastSidersTheSeries/videos/1168035630252433/"
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={e => {
+                trackCustomEvent({
+                  // string - required - The object that was interacted with (e.g.video)
+                  category: "Trailer",
+                  // string - required - Type of interaction (e.g. 'play')
+                  action: "Share on Facebook",
+                  // string - optional - Useful for categorizing events (e.g. 'Spring Campaign')
+                  label: `Season ${featuredSeason.season}`,
+                })
+              }}
+              color="facebook"
+              size="tiny"
+            >
+              <Icon name="facebook" /> Share on Facebook
+            </Button>
+            <Button
+              as="a"
+              href="https://www.youtube.com/watch?v=hdoeDtY6XQc"
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={e => {
+                trackCustomEvent({
+                  // string - required - The object that was interacted with (e.g.video)
+                  category: "Trailer",
+                  // string - required - Type of interaction (e.g. 'play')
+                  action: "View on YouTube",
+                  // string - optional - Useful for categorizing events (e.g. 'Spring Campaign')
+                  label: `Season ${featuredSeason.season}`,
+                })
+              }}
+              color="youtube"
+              size="tiny"
+            >
+              <Icon name="youtube" /> View on YouTube
+            </Button>
+          </Container>
+          <p style={{ paddingTop: "0.5em" }}>
+            The fourth and final season of Eastsiders will be released on
+            December 1 for Streaming on Netflix. Until then, get a glimpse of
+            whats coming by watching the Season 4 trailer above. Share with your
+            friends using the provided social sharing buttons.
+            <br />
+            <br />
+            All 3 seasons of Eastsiders are available to{" "}
+            <OutboundLink href={featuredSeason.stream[0].url}>
+              stream on Netflix
+            </OutboundLink>
+            . Catch up or rewatch all the episodes.
+          </p>
         </Featured>
         <Side1>
           <Header>Merch</Header>
           <MerchCard item={randomMerch()} fluid />
           <p>
-            Check out the <Link to="/merch/">Merch Table</Link>.
+            Check out the <Link to="/merch/">Merch Table</Link> for more items.
           </p>
         </Side1>
         <Side2>
